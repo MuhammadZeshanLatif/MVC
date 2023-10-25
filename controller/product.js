ed1020e419750ad7b3c2df723950ce9ac803ec33
Muhammad Zeeshan Latif <zeeshanmuhammad42201@gmail.com>
2023-10-26 01:46:19 +0500

    MVC

--- controller/product.js
+++ controller/product.js
@@ -1,41 +1,38 @@
 const fs=require('fs');
-const data=JSON.parse(fs.readFileSync('data.json','utf-8'));;
-exports.startServer=(req,res)=>
-{
-    res.send("Server start....")
-}
+const _products=JSON.parse(fs.readFileSync('data.json','utf-8'));;
+const products=_products.products;
 exports.getAllProducts=(req,res)=>{
-    res.json(data);
+    res.json(products);
 }
 exports.getProducts=(req,res)=>{
     console.log(req.body);
-    data.push(req.body);
+    products.push(req.body);
     res.status(201).json({type:'POST'});
 }
 exports.getSingleProduct=(req,res)=>{
     const id = +req.params.id;
     console.log("these :"+req.params.id);
-    //const product=data.find(p=>p.id===id)
-    const product=data.find(p=>p.id===id)
+    //const product=products.find(p=>p.id===id)
+    const product=products.find(p=>p.id===id)
     res.status(200).json(product);
 }
 exports.replaceProduct =(req,res)=>{
     const id = +req.params.id;
-    const productIndex = data.findIndex(p=>p.id===id);
-    data.splice(productIndex,1,{...req.body,id:id});
+    const productIndex = products.findIndex(p=>p.id===id);
+    products.splice(productIndex,1,{...req.body,id:id});
     res.status(201).json()
  }
  exports.updateProduct=(req,res)=>{
     const id = +req.params.id;
-    const productIndex = data.findIndex(p=>p.id===id);
-    const singleProduct=data[productIndex]
-    data.splice(productIndex,1,{...singleProduct,...req.body});
+    const productIndex = products.findIndex(p=>p.id===id);
+    const singleProduct=products[productIndex]
+    products.splice(productIndex,1,{...singleProduct,...req.body});
     res.status(201).json()
 }
 exports.deleteProduct=(req,res)=>{
     const id = +req.params.id;
-    const productIndex = data.findIndex(p=>p.id===id);
-    const singleProduct=data[productIndex];
-    data.splice(productIndex,1);
+    const productIndex = products.findIndex(p=>p.id===id);
+    const singleProduct=products[productIndex];
+    products.splice(productIndex,1);
     res.status(201).json('Product delete :'+singleProduct);
 }
--- index.js
+++ index.js
@@ -1,32 +1,14 @@
-const productController=require('./controller/product.js');
 //make server with express
 const express = require('express');
 const morgan = require('morgan');
 const server = express();
 //Make a router
-const productRouter=express.Router();
-server.use('/api',productRouter);
-server.use(morgan('default'))
-server.use(express.static('public'));
-server.use(express.json())
-
-//Crud
-//1)Create 
-productRouter
-.post('/products',productController.getProducts)
-//2)Read
-//Read all product
-.get('/products',productController.getAllProducts)
-//Read Single Product 
-.get('/products/:id',productController.getSingleProduct)
-//3)Update
-//Update full product
-.put('/products/:id',productController.replaceProduct)
-//Update individual property
-.patch('/products/:id',productController.updateProduct)
-//4)Deleta
-.delete('/products/:id',productController.deleteProduct)
-//Model-View-controller
-//Server start
-server.get('/demo',productController.startServer);
-server.listen(8080)
+const productRouter=require('./router/productsRouter.js')
+const usersRouter=require('./router/usersRouter.js')
+server
+.use('/products',productRouter.router)
+.use('/users',usersRouter.router)
+.use(morgan('default'))
+.use(express.static('public'))
+.use(express.json())
+.listen(8080);
