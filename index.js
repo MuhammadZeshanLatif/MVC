4006c76923bb162798bc0b81fefa339e40d6e74a
Muhammad Zeeshan Latif <zeeshanmuhammad42201@gmail.com>
2023-10-28 15:10:52 +0500

    MVC .env

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-//make server with express
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const server = express();
 //Make a router
-const productRouter=require('./router/productsRouter.js')
-const usersRouter=require('./router/usersRouter.js')
+const productRouter=require('./router/productsRouter.js');
+const usersRouter=require('./router/usersRouter.js');
+console.log('env',process.env.DB_PASSWORD);
 server
 .use('/products',productRouter.router)
 .use('/users',usersRouter.router)
 .use(morgan('default'))
-.use(express.static('public'))
+.use(express.static(process.env.PUBLIC_DIR))
 .use(express.json())
-.listen(8080);
+.listen(process.env.PORT);
